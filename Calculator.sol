da55bc246f4403a73a758b6d72e2c0404475a07f
Divya Nautiyal <divyanautiyal07@gmail.com>
2022-07-12 12:36:56 +0530

    Subtract function implemented

--- Calculator.sol
+++ Calculator.sol
@@ -6,4 +6,8 @@ contract Calculator{
     function add(int _num1, int _num2) public pure returns(int){
         return _num1 + _num2;
     }
+    function subtract(int _num1, int _num2) public pure returns(int){
+        return _num1  - _num2;
+    }
+
 }
